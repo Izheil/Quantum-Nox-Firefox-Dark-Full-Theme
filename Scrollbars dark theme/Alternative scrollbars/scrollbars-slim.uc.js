5bf344ef36b2dde4fed00338f1d3f5072b5c341f
Izheil <33794458+Izheil@users.noreply.github.com>
2018-01-03 10:50:50 +0100

    Update scrollbars-slim.uc.js

--- Scrollbars dark theme/Alternative scrollbars/scrollbars-slim.uc.js
+++ Scrollbars dark theme/Alternative scrollbars/scrollbars-slim.uc.js
@@ -20,7 +20,7 @@ scrollbar[orient="vertical"] {
 	max-width: 12px !important;
 	min-width: 12px !important;
 
-	background: linear-gradient(to right, #222, #444, #222) !important;
+	background: linear-gradient(to right, #222, #444) !important;
 	background-size: 12px 12px !important;
 	background-repeat: repeat-y !important;
 	background-position: 50% 0% !important;
@@ -32,7 +32,7 @@ scrollbar[orient="horizontal"] {
 	max-height: 12px !important;
 	min-height: 12px !important;
   
-	background: linear-gradient(#222, #444, #222) !important;
+	background: linear-gradient(#222, #444) !important;
 	background-size: 12px 12px !important;
 	background-repeat: repeat-x  !important;
 	background-position: 0% 50% !important;
