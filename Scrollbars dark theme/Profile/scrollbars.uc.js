c1fc37351b239c8fca36a3190ff604b7f6da4b64
Izheil <33794458+Izheil@users.noreply.github.com>
2018-01-03 10:55:16 +0100

    Update scrollbars.uc.js

--- Scrollbars dark theme/Profile/scrollbars.uc.js
+++ Scrollbars dark theme/Profile/scrollbars.uc.js
@@ -3,7 +3,6 @@
 scrollbar {
 	z-index: 2147483647 !important;
 	position: relative !important;
-	transition: all 0s !important; 
 }
 
 scrollbar,
@@ -58,14 +57,13 @@ scrollbar thumb {
 	background: transparent !important;
 	border: 2px solid rgba(0,255,255,0) !important;
 	box-shadow: 0 0 0 8px #888 inset !important;
-	transition: all 0s !important; 
+	transition: all .4s !important; 
 	opacity: 1 !important; 
 }
 
 scrollbar:hover thumb,
 scrollbar thumb:active {
 	box-shadow: 0 0 0 8px #46b inset !important;
-	transition: all .3s !important; 
 }
 
 scrollbar thumb:active {
