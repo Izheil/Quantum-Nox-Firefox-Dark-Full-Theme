0471b7d37f68b71b3631b4ca2a58b8f0139dd387
Izheil <33794458+Izheil@users.noreply.github.com>
2018-05-26 13:26:30 +0200

    Changed when the tab position indicator appears
    Now the tab position indicator should only appear when there is more than 1 row of tabs open (or past 22 tabs open), otherwise, the default ordering animation should apply.

--- Scrollbars & tooltips dark theme/Profile/MultiRowTabLiteforFx.uc.js
+++ Scrollbars & tooltips dark theme/Profile/MultiRowTabLiteforFx.uc.js
@@ -79,9 +79,10 @@ function zzzz_MultiRowTabLite() {
         }
         return tabs.length;
     };
+    // This sets when to apply the fix (by default a new row starts after the 23th open tab, unless you changed the min-size of tabs)
+    gBrowser.tabContainer.ondragstart = function(){if(gBrowser.tabContainer.childNodes.length >= 23) {
 
     gBrowser.tabContainer._getDropEffectForTabDrag = function(event){return "";}; // multirow fix: to make the default "dragover" handler does nothing
-
     gBrowser.tabContainer._onDragOver = function(event) {
         event.preventDefault();
         event.stopPropagation();
@@ -155,7 +156,7 @@ function zzzz_MultiRowTabLite() {
         }
     };
     gBrowser.tabContainer.addEventListener("drop", function(event){this.onDrop(event);}, true);
-}
+}}}
 
 // copy of the original and overrided _getDropEffectForTabDrag method
 function orig_getDropEffectForTabDrag(event) {
