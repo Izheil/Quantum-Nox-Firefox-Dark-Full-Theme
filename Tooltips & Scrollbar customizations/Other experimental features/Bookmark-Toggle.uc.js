df8c2930dab64e34cd0beada7639341c9f7c330c
Izheil <33794458+Izheil@users.noreply.github.com>
2018-12-27 21:37:06 +0100

    Updated the background color of the urlbar to be taken from it's default variable instead of an external rule.

---
+++ Tooltips & Scrollbar customizations/Other experimental features/Bookmark-Toggle.uc.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name           Bookmark-toggle.uc.js
+// @namespace      http://https://github.com/Izheil/Quantum-Nox-Firefox-Dark-Full-Theme
+// @description    Bookmarks toolbar toggle on keydown
+// @include        main
+// @compatibility  Firefox 64
+// @author         Izheil
+// @version        27/12/2018 15:05 Firefox 64
+// ==/UserScript==
+window.addEventListener("keydown", bookmarkToggle, false);
+
+// This assumes you have added the "visibility: collapse !important" rule on your userchrome.css for #PersonalToolbar
+// If you want them to be toggled by default, change this to true instead.
+var toggled = false;
+
+var css =`
+	  #PersonalToolbar {visibility: collapse !important}`;
+var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
+var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
+sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+
+// If you want to use a combination of keys instead than one, include evt.key == "your key here" with
+// a && inclusion (for example evt.key == "F2" && evt.key =="a" would trigger the toggling when both
+// F2 and a buttons are pressed). For ctrl, shift, or alt keys, just use evt.ctrlKey, evt.shiftKey, and evt.altKey instead.
+function bookmarkToggle(evt) {
+	// Input the key you want to use here
+    if (evt.key == "F2" && toggled == true) {
+     var css =`
+	  #PersonalToolbar {visibility: collapse !important}`;
+	  toggled = false}
+	// Input the key you want to use here
+	else if (evt.key == "F2" && toggled == false) {
+     var css =`
+      #PersonalToolbar {visibility: visible !important}`;
+	  toggled = true}
+var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
+var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
+sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+}
+
+
---
+++ Tooltips & Scrollbar customizations/Other experimental features/README.md
@@ -0,0 +1,7 @@
+<h2>Other experimental features</h2>
+<p>Here go any other experimental feature  that isn't the main focus of the theme and their description. You shouldn't really need to install these unless you want some very specific function.</p>
+<p>There are probably better alternatives out there, so you should only use these if you really couldn't find it anywhere else (since, as mentioned, these are experimental)</p>
+
+<h3>Bookmarks-toggle.uc.js</h3>
+<p>Toggles visibility of the bookmarks toolbar when pressing F2 (you can change which keys in the file). You'll need to add this rule to your userchrome:</p>
+<pre>#PersonalToolbar {visibility: collapse !important}</pre>
