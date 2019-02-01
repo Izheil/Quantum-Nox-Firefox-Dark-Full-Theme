5e810a908af6702b827d48832a1476386d598229
Izheil <33794458+Izheil@users.noreply.github.com>
2019-02-01 22:59:15 +0100

    Fixed the navigator toolbox autohide to work with menu bar enabled

--- Tooltips, Scrollbars & Multirow tabs/Other experimental features/Navigator-toolbox-autohide.uc.js
+++ Tooltips, Scrollbars & Multirow tabs/Other experimental features/Navigator-toolbox-autohide.uc.js
@@ -5,22 +5,28 @@
 // @include        main
 // @compatibility  Firefox 65
 // @author         Izheil
-// @version        01/31/2019 10:32 First version
+// @version        01/02/2019 22:48 Revised version to hide all content inside the navigation bar
+// @version        31/01/2019 10:32 First version
 // ==/UserScript==
 document.getElementById("fullscr-toggler").onmouseover = function() {
     var css =`
-      #PersonalToolbar, #titlebar {visibility: visible !important}
-      #nav-bar {margin-top: 0 !important}
+      #navigator-toolbox > *:not(#nav-bar) {visibility: visible !important}
+      #main-window[sizemode="maximized"] #nav-bar,
+      #main-window[sizemode="fullscreen"] #nav-bar,
+      #main-window[sizemode="normal"] #nav-bar {margin-top: 0 !important}
       #fullscr-toggler {display: none !important}`;
     var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
     var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
     sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
 }
 
+// You may need to change these margins if you are using an older windows version.
 document.getElementById("browser").onmouseover = function() {
     var css =`
-      #PersonalToolbar, #titlebar {visibility: collapse !important}
-      #nav-bar {margin-top: -33px !important}
+      #navigator-toolbox > *:not(#nav-bar) {visibility: collapse !important}
+      #main-window[sizemode="maximized"] #nav-bar {margin-top: -33px !important}
+      #main-window[sizemode="fullscreen"] #nav-bar {margin-top: -41px !important}
+      #main-window[sizemode="normal"] #nav-bar {margin-top: -38px !important}
       #fullscr-toggler {display: block !important}`;
     var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
     var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
