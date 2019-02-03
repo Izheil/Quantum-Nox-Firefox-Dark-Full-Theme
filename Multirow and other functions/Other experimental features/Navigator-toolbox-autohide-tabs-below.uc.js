9def1373d3b1d599e18de90f15ea8fc98684d22d
Izheil <33794458+Izheil@users.noreply.github.com>
2019-02-03 19:38:09 +0100

    Updated navigator-box autohide to work with all monitor displays by default

--- Multirow and other functions/Other experimental features/Navigator-toolbox-autohide-tabs-below.uc.js
+++ Multirow and other functions/Other experimental features/Navigator-toolbox-autohide-tabs-below.uc.js
@@ -5,70 +5,59 @@
 // @include        main
 // @compatibility  Firefox 65
 // @author         Izheil
+// @version        03/02/2019 19:35 Rewrote the code to use more javascript instead of pure CSS
 // @version        02/02/2019 03:34 Included a fix for tabs-below to be compatible with autohide
 // @version        02/02/2019 22:48 Gave a bigger toggler space for autohide on resized window
 // @version        01/02/2019 22:48 Revised version to hide all content inside the navigation bar
 // @version        31/01/2019 10:32 First version
 // ==/UserScript==
-    var css =`
-      #nav-bar {-moz-box-ordinal-group: 2 !important}
-      #PersonalToolbar {-moz-box-ordinal-group: 1 !important}
-      #titlebar {-moz-box-ordinal-group: 3 !important}
+// We define all variables first
+var navBox = document.getElementById("navigator-toolbox"),
+  bkmBox = document.getElementById("PersonalToolbar"),
+  titleBox = document.getElementById("toolbar-menubar"),
+  winState = window.windowState,
+  fsToggler = document.getElementById("fullscr-toggler");
 
-      #navigator-toolbox {margin-top: 40px !important}
-      /* ^ Change the margin to 10px if you don't want to use the menu bar on top ^ */
+var css =`
+  #nav-bar {-moz-box-ordinal-group: 2 !important}
+  #PersonalToolbar {-moz-box-ordinal-group: 1 !important}
+  #titlebar {-moz-box-ordinal-group: 3 !important}
 
-      /* This changes the menu bar on top of the screen intead of over the tab bar */
-      #toolbar-menubar{
-        position: fixed !important; 
-        width: 100% !important;
-        top: 0 !important;
-        margin-top: 7px !important}
+  #navigator-toolbox {margin-top: 40px !important}
+  /* ^ Change the margin to 10px if you don't want to use the menu bar on top ^ */
 
-      #toolbar-menubar .titlebar-buttonbox-container {
-        float: right !important}
+  /* This changes the menu bar on top of the screen intead of over the tab bar */
+  #toolbar-menubar{
+    position: fixed !important; 
+    width: 100% !important;
+    top: 0 !important;
+    margin-top: 7px !important}
 
-      #window-controls {
-        position: fixed !important;
-        top: 0 !important; 
-        right: 0 !important}
-        `;
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
-    sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+  #toolbar-menubar .titlebar-buttonbox-container {
+    float: right !important}
 
-document.getElementById("fullscr-toggler").onmouseover = function() {
-    var css =`
-      #navigator-toolbox > *:not(#nav-bar), #toolbar-menubar {visibility: visible !important}
-      #main-window[sizemode="maximized"] #nav-bar,
-      #main-window[sizemode="fullscreen"] #nav-bar,
-      #main-window[sizemode="normal"] #nav-bar {margin-top: 0 !important}
+  #window-controls {
+    position: fixed !important;
+    top: 0 !important; 
+    right: 0 !important}
+    `;
+var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
+var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
+sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
 
-      #toolbar-menubar {display: block !important}
-      #fullscr-toggler {display: none !important}`;
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
-    sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+// This shows the navigation bar when hovering over the fullscr-toggler element (div on top of screen)
+fsToggler.onmouseover = function() {
+  navBox.style.visibility = "visible";
+  bkmBox.style.opacity = "1";
+  titleBox.style.opacity = "1";
+  fsToggler.style.display = "none";
 }
 
-// You may need to change these margins if you are using an older windows version.
+// This hides the navigation bar when hovering over the web area
 document.getElementById("browser").onmouseover = function() {
-    var css =`
-      #navigator-toolbox > *:not(#nav-bar), #toolbar-menubar {visibility: collapse !important}
-      #main-window[sizemode="maximized"] #nav-bar {margin-top: -73px !important}
-      #main-window[sizemode="fullscreen"] #nav-bar {margin-top: -81px !important}
-      #main-window[sizemode="normal"] #nav-bar {margin-top: -78px !important}
-
-      #toolbar-menubar {display: none !important}
-
-      #main-window:not([sizemode="normal"]) #fullscr-toggler {
-        display: block !important; 
-        height: 1px !important}
-        
-      #main-window[sizemode="normal"] #fullscr-toggler {
-        display:block !important; 
-        height: 5px !important}`;
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
-    sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+    navBox.style.visibility = "collapse";
+    bkmBox.style.opacity = "0";
+    titleBox.style.opacity = "0";
+    fsToggler.style.display = "block";
+    fsToggler.style.height = "20px";
 }
--- Multirow and other functions/Other experimental features/Navigator-toolbox-autohide.uc.js
+++ Multirow and other functions/Other experimental features/Navigator-toolbox-autohide.uc.js
@@ -5,38 +5,29 @@
 // @include        main
 // @compatibility  Firefox 65
 // @author         Izheil
+// @version        03/02/2019 19:34 Rewrote the code on Javascript only
 // @version        02/02/2019 22:48 Gave a bigger toggler space for autohide on resized window
 // @version        01/02/2019 22:48 Revised version to hide all content inside the navigation bar
 // @version        31/01/2019 10:32 First version
 // ==/UserScript==
-document.getElementById("fullscr-toggler").onmouseover = function() {
-    var css =`
-      #navigator-toolbox > *:not(#nav-bar) {visibility: visible !important}
-      #main-window[sizemode="maximized"] #nav-bar,
-      #main-window[sizemode="fullscreen"] #nav-bar,
-      #main-window[sizemode="normal"] #nav-bar {margin-top: 0 !important}
-      #fullscr-toggler {display: none !important}`;
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
-    sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+
+// We define all variables first
+var navBox = document.getElementById("navigator-toolbox"),
+	bkmBox = document.getElementById("PersonalToolbar"),
+	winState = window.windowState,
+	fsToggler = document.getElementById("fullscr-toggler");
+
+// This shows the navigation bar when hovering over the fullscr-toggler element (div on top of screen)
+fsToggler.onmouseover = function() {
+	navBox.style.visibility = "visible";
+	bkmBox.style.opacity = "1";
+	fsToggler.style.display = "none";
 }
 
-// You may need to change these margins if you are using an older windows version.
+// This hides the navigation bar when hovering over the web area
 document.getElementById("browser").onmouseover = function() {
-    var css =`
-      #navigator-toolbox > *:not(#nav-bar) {visibility: collapse !important}
-      #main-window[sizemode="maximized"] #nav-bar {margin-top: -33px !important}
-      #main-window[sizemode="fullscreen"] #nav-bar {margin-top: -41px !important}
-      #main-window[sizemode="normal"] #nav-bar {margin-top: -38px !important}
-
-      #main-window:not([sizemode="normal"]) #fullscr-toggler {
-        display: block !important; 
-        height: 1px !important}
-        
-      #main-window[sizemode="normal"] #fullscr-toggler {
-        display:block !important; 
-        height: 5px !important}`;
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
-    sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+  	navBox.style.visibility = "collapse";
+  	bkmBox.style.opacity = "0";
+    fsToggler.style.display = "block";
+    fsToggler.style.height = "10px";
 }
