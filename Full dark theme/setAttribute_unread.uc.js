4005a99475cc75d91d644f08aa8147fcb96b12fe
Izheil <33794458+Izheil@users.noreply.github.com>
2019-05-25 09:20:34 +0200

    Update setAttribute_unread.uc.js

--- Full dark theme/setAttribute_unread.uc.js
+++ Full dark theme/setAttribute_unread.uc.js
@@ -1,19 +1,18 @@
 (function(){
-	if (location.href != 'chrome://browser/content/browser.xul' && 
-		!gBrowser.tabContainer.childNode.tabAnimationsInProgress &&
-		Services.prefs.getBoolPref("toolkit.cosmeticAnimations.enabled")) return;
+	if (location.href != 'chrome://browser/content/browser.xul') return;
 
 	let func = {
+		
 			add : function(e){
 				e.target.setAttribute('unread', 'true');
 			},
 			remove : function(e){
 				e.target.removeAttribute('unread');
 			}
-	}
-	gBrowser.tabContainer.addEventListener('TabOpen', func.add, false);
-	gBrowser.tabContainer.addEventListener('TabSelect', func.remove, false);
-	gBrowser.tabContainer.addEventListener('TabClose', func.remove, false);
+	};
+		gBrowser.tabContainer.addEventListener('TabOpen', func.add, false);
+		gBrowser.tabContainer.addEventListener('TabSelect', func.remove, false);
+		gBrowser.tabContainer.addEventListener('TabClose', func.remove, false);
 	window.addEventListener('unload', function uninit(){
 		gBrowser.tabContainer.removeEventListener('TabOpen', func.add, false);
 		gBrowser.tabContainer.removeEventListener('TabSelect', func.remove, false);
