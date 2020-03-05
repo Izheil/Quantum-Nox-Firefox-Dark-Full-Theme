d6082c290fe34d375ef6598d7f7fbafde419ab32
Izheil <33794458+Izheil@users.noreply.github.com>
2020-03-06 00:34:46 +0100

    Fixed tab line issues

--- Multirow and other functions/Multirow tabs/MultiRowTabLiteforFx.uc.js
+++ Multirow and other functions/Multirow tabs/MultiRowTabLiteforFx.uc.js
@@ -5,7 +5,7 @@
 // @include        main
 // @compatibility  Firefox 75
 // @author         Alice0775, Endor8, TroudhuK, Izheil
-// @version        05/03/2020 19:40 Removed redundant CSS code that is not necessary anymore
+// @version        06/03/2020 00:30 Fixed an issue with tab lines
 // @version        12/02/2020 03:30 Fixed some issue with the min/resize/close buttons
 // @version        18/01/2020 02:39 Added a fix for people who always spoof their useragent
 // @version        13/01/2020 05:01 Fixed the tab drop indicator on FF72+
@@ -55,6 +55,11 @@ function zzzz_MultiRowTabLite() {
 
     #tabbrowser-tabs .tab-background, #tabbrowser-tabs .tabbrowser-tab {
         min-height: var(--tab-min-height) !important}
+    
+    #main-window[sizemode="maximized"] #navigator-toolbox,
+    #main-window[sizemode="fullscreen"] #navigator-toolbox {transform: translate(0, -1px) !important}
+
+    .toolbar-items {margin-top: 1px !important}
     
 	.tab-stack {width: 100%}
 
