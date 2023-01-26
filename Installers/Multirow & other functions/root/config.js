e62fdf4be5acf9a4733507ac260d6add123d74f0
Izheil <33794458+Izheil@users.noreply.github.com>
2023-01-27 00:47:14 +0100

    Update root folder files

--- Installers/Multirow & other functions/root/config.js
+++ Installers/Multirow & other functions/root/config.js
@@ -1,22 +1,18 @@
 // skip 1st line
 lockPref('xpinstall.signatures.required', false);
-
-Object = Cu.getGlobalForObject(Cu).Object;
-const { freeze } = Object;
-Object.freeze = obj => {
-  if (Components.stack.caller.filename != 'resource://gre/modules/AppConstants.jsm')
-    return freeze(obj);
-
-  obj.MOZ_REQUIRE_SIGNING = false;
-  Object.freeze = freeze;
-  return freeze(obj);
-}
+lockPref('extensions.install_origins.enabled', false);
 
 try {
-  let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
+  const cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
   cmanifest.append('utils');
   cmanifest.append('chrome.manifest');
   Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
+
+  const objRef = ChromeUtils.import('resource://gre/modules/addons/AddonSettings.jsm');
+  const temp = Object.assign({}, Object.getOwnPropertyDescriptors(objRef.AddonSettings), {
+    REQUIRE_SIGNING: { value: false }
+  });
+  objRef.AddonSettings = Object.defineProperties({}, temp);
 
   Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
 } catch (ex) {};
